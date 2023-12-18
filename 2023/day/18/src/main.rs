2b3c7037b391f6fa2877ec3021de58061db750c8
Philip Taylor <philip@zaynar.co.uk>
2023-12-18 23:52:11 +0000

    Day 18: part 2

--- 2023/day/18/src/main.rs
+++ 2023/day/18/src/main.rs
@@ -1,20 +1,39 @@
 use std::{fs, collections::HashMap};
 
+fn sign(a: f64) -> f64 {
+    if a == 0.0 {
+        0.0
+    } else {
+        a.signum()
+    }
+}
+
 fn main() {
     let input: Vec<_> = fs::read_to_string("input").unwrap().lines().map(|line| {
         let mut tokens = line.split_ascii_whitespace();
         let dir = tokens.next().unwrap().to_owned();
         let dist = tokens.next().unwrap().parse::<i32>().unwrap();
         let colour = tokens.next().unwrap()[2..8].to_owned();
-        (dir, dist, colour)
-    }).collect();
-    // println!("{:?}", input);
 
-    let mut minpos = (0, 0);
-    let mut maxpos = (0, 0);
-    let mut trench: HashMap<(i32, i32), bool> = HashMap::new();
+        let dir2 = (match colour.chars().nth(5).unwrap() {
+            '0' => "R",
+            '1' => "D",
+            '2' => "L",
+            '3' => "U",
+            _ => unreachable!(),
+        }).to_owned();
+        let dist2 = i64::from_str_radix(&colour[0..5], 16).unwrap();
+
+        // (dir, dist)
+        (dir2, dist2)
+    }).collect();
+    println!("{:?}", input);
+
+    let mut verts = Vec::new();
+    let mut hedges = Vec::new();
     let mut pos = (0, 0);
-    for (dir, dist, colour) in input {
+    verts.push(pos);
+    for (dir, dist) in input {
         let (dx, dy) = match dir.as_str() {
             "U" => (0, -1),
             "D" => (0, 1),
@@ -22,37 +41,82 @@ fn main() {
             "R" => (1, 0),
             _ => unreachable!(),
         };
-        for i in 0..dist {
-            trench.insert(pos, true);
-            pos = (pos.0 + dx, pos.1 + dy);
-        }
-
-        minpos = (minpos.0.min(pos.0), minpos.1.min(pos.1));
-        maxpos = (maxpos.0.max(pos.0), maxpos.1.max(pos.1));
+        // let dist = if dx == 1 || dy == 1 { dist + 1 } else { dist - 1 };
+        // if dx != 0 {
+        //     hedges.push((pos.1, pos.0, pos.0 + dx * dist));
+        // }
+        pos = (pos.0 + dx * dist, pos.1 + dy * dist);
+        verts.push(pos);
     }
 
-    println!("{:?} {:?}", minpos, maxpos);
+    println!("{:?}", verts);
 
-    let mut dug = 0;
-    for y in minpos.1..=maxpos.1 {
-        let mut inside = false;
-        for x in minpos.0..=maxpos.0 {
-            // print!("{}", if *trench.get(&(x, y)).unwrap_or(&false) { "#" } else { "." });
+    let mut bpos = (0.0, 0.0);
+    for (i, vs) in verts.iter().
+    take(verts.len() - 1).
+    cycle().take(verts.len() + 2).
+    map(|(x, y)| (*x as f64, *y as f64)).collect::<Vec<_>>().windows(3)
+    .enumerate() {
+        let dir = -(
+            (vs[2].0 - vs[1].0) * (vs[1].1 - vs[0].1) -
+            (vs[2].1 - vs[1].1) * (vs[1].0 - vs[0].0)
+        ).signum();
+        // println!("{:?} {:?} {:?} {}", vs[0], vs[1], vs[2], dir);
 
-            if *trench.get(&(x, y)).unwrap_or(&false) {
-                print!("#");
-                dug += 1;
-                if *trench.get(&(x, y+1)).unwrap_or(&false) {
-                    inside = !inside;
-                }
-              } else if inside {
-                print!("@");
-                dug += 1;
-            } else {
-                print!(".");
-            }
+        let mut npos = bpos;
+
+        if dir > 0.0 {
+            // println!("# {} + {} + {}", vs[1].0 + 0.5, sign(vs[1].0 - vs[0].0) * 0.5, sign(vs[1].0 - vs[2].0) * 0.5);
+            npos = (
+                vs[1].0 + 0.5 + sign(vs[1].0 - vs[0].0) * 0.5 + sign(vs[1].0 - vs[2].0) * 0.5,
+                vs[1].1 + 0.5 + sign(vs[1].1 - vs[0].1) * 0.5 + sign(vs[1].1 - vs[2].1) * 0.5,
+            )
+            // if vs[0].1 == vs[1].1 {
+            //     npos.0 += vs[1].0 - vs[0].0 + (vs[1].0 - vs[0].0).signum();
+            // } else {
+            //     npos.1 += vs[1].1 - vs[0].1 + (vs[1].1 - vs[0].1).signum();
+            // }
+            // if vs[0].1 == vs[1].1 {
+            //     npos.0 += vs[1].0 - vs[0].0 + (vs[1].0 - vs[0].0).signum();
+            // } else {
+            //     npos.1 += vs[1].1 - vs[0].1 + (vs[1].1 - vs[0].1).signum();
+            // }
+        } else {
+            npos = (
+                vs[1].0 + 0.5 - sign(vs[1].0 - vs[0].0) * 0.5 - sign(vs[1].0 - vs[2].0) * 0.5,
+                vs[1].1 + 0.5 - sign(vs[1].1 - vs[0].1) * 0.5 - sign(vs[1].1 - vs[2].1) * 0.5,
+            )
+            // if vs[0].1 == vs[1].1 {
+            //     npos.0 += vs[1].0 - vs[0].0 - (vs[1].0 - vs[0].0).signum();
+            // } else {
+            //     npos.1 += vs[1].1 - vs[0].1 - (vs[1].1 - vs[0].1).signum();
+            // }
         }
-        println!();
+
+        if i > 0 && bpos.1 == npos.1 {
+            hedges.push((bpos.1 as i64, bpos.0 as i64, npos.0 as i64));
+        }
+
+        println!("{:?}-{:?}", bpos, npos);
+        bpos = npos;
     }
-    println!("Answer 1: {}", dug);
+
+    hedges.sort();
+
+    println!("{:?}", hedges);
+
+    let mut area = 0;
+    let mut active = 0;
+    let mut prevy = 0;
+    for (y, x0, x1) in hedges {
+        area += active * (y as i64 - prevy);
+        active += (x1 - x0) as i64;
+        // area += (x1 - x0).abs();
+        // area += ((x1 - x0).abs() - 1);
+        // active += (x1 - x0).signum() * ((x1 - x0).abs() + 1);
+        println!("{} {} {}", y, area, active);
+        prevy = y as i64;
+    }
+
+    println!("{}", area);
 }
