ddb37c2a8dd9f809a97f89d2ffa76ff9f760bf7c
Philip Taylor <philip@zaynar.co.uk>
2024-12-02 22:36:48 +0000

    2019 day 4

--- 2019/04/src/main.rs
+++ 2019/04/src/main.rs
@@ -1,21 +1,40 @@
-fn run(title: &str, input: &str) {
-    let data: Vec<Vec<i32>> = input
-        .lines()
-        .map(|line| {
-            line.split_ascii_whitespace()
-                .map(|n| str::parse(n).unwrap())
-                .collect()
-        })
-        .collect();
-
-    println!("{} part 1: {}", title, "TODO");
-
-    println!("{} part 2: {}", title, "TODO");
-}
-
-const INPUT_DEMO: &str = "";
-
 fn main() {
-    run("demo", INPUT_DEMO);
-    run("input", &std::fs::read_to_string("04/input.txt").unwrap());
+    let mut part1 = 0;
+    for i in 284639..=748759 {
+        let digits = [
+            (i / 100000) % 10,
+            (i / 10000) % 10,
+            (i / 1000) % 10,
+            (i / 100) % 10,
+            (i / 10) % 10,
+            i % 10,
+        ];
+
+        if digits.windows(2).any(|w| w[0] == w[1]) && digits.windows(2).all(|w| w[0] <= w[1]) {
+            part1 += 1;
+        }
+    }
+
+    let mut part2 = 0;
+    for i in 284639..=748759 {
+        let digits = [
+            -1,
+            (i / 100000) % 10,
+            (i / 10000) % 10,
+            (i / 1000) % 10,
+            (i / 100) % 10,
+            (i / 10) % 10,
+            i % 10,
+            i32::MAX,
+        ];
+
+        if digits.windows(4).any(|w| w[0] != w[1] && w[1] == w[2] && w[2] != w[3]) && digits.windows(2).all(|w| w[0] <= w[1]) {
+            part2 += 1;
+        }
+    }
+
+
+    println!("part 1: {}", part1);
+
+    println!("part 2: {}", part2);
 }
