f5bf193a10acdbd416653d546a6fc80322099111
Philip Taylor <philip@zaynar.co.uk>
2024-12-07 12:02:28 +0000

    2024 day 7

--- 2024/07/src/main.rs
+++ 2024/07/src/main.rs
@@ -1,19 +1,60 @@
+use itertools::Itertools;
+
+fn test1(r: i64, start: i64, rest: &[i64]) -> bool {
+    if rest.is_empty() {
+        return start == r;
+    }
+
+    return test1(r, start + rest[0], &rest[1..])
+    || test1(r, start * rest[0], &rest[1..]);
+}
+
+fn test2(r: i64, start: i64, rest: &[i64]) -> bool {
+    if rest.is_empty() {
+        return start == r;
+    }
+
+    return test2(r, start + rest[0], &rest[1..])
+    || test2(r, start * rest[0], &rest[1..])
+    // || test2(r, (start.to_string() + &rest[0].to_string()).parse().unwrap(), &rest[1..]);
+    || test2(r, start * 10i64.pow(rest[0].ilog10() + 1) + rest[0], &rest[1..]);
+}
+
 fn run(title: &str, input: &str) {
-    let data: Vec<Vec<i32>> = input
+    let data: Vec<(i64, Vec<i64>)> = input
         .lines()
         .map(|line| {
-            line.split_ascii_whitespace()
-                .map(|n| str::parse(n).unwrap())
-                .collect()
+            let (r, ops) = line.split_once(": ").unwrap();
+            (r.parse().unwrap(), ops.split_whitespace().map(|n| n.parse().unwrap()).collect())
         })
         .collect();
 
-    println!("{} part 1: {}", title, "TODO");
+    let part1 = data.iter().filter(|(r, ops)|
+        test1(*r, ops[0], &ops[1..])
+    ).collect_vec();
 
-    println!("{} part 2: {}", title, "TODO");
+    let part2 = data.iter().filter(|(r, ops)|
+        test2(*r, ops[0], &ops[1..])
+    ).collect_vec();
+
+    // println!("{:?}", data);
+    // println!("{:?}", part1);
+
+    println!("{} part 1: {}", title, part1.iter().map(|(r, ops)| *r).sum::<i64>());
+
+    println!("{} part 2: {}", title, part2.iter().map(|(r, ops)| *r).sum::<i64>());
 }
 
-const INPUT_DEMO: &str = "";
+const INPUT_DEMO: &str = "190: 10 19
+3267: 81 40 27
+83: 17 5
+156: 15 6
+7290: 6 8 6 15
+161011: 16 10 13
+192: 17 8 14
+21037: 9 7 18 13
+292: 11 6 16 20
+";
 
 fn main() {
     run("demo", INPUT_DEMO);
