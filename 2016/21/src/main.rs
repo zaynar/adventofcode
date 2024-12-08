48f8aa05ba03bdff6c438ecbfb98a506df716f96
Philip Taylor <philip@zaynar.co.uk>
2024-12-08 00:38:01 +0000

    2016 day 21, 22

--- 2016/21/src/main.rs
+++ 2016/21/src/main.rs
@@ -1,21 +1,114 @@
-fn run(title: &str, input: &str) {
-    let data: Vec<Vec<i32>> = input
-        .lines()
-        .map(|line| {
-            line.split_ascii_whitespace()
-                .map(|n| str::parse(n).unwrap())
-                .collect()
-        })
-        .collect();
+use itertools::Itertools;
 
-    println!("{} part 1: {}", title, "TODO");
-
-    println!("{} part 2: {}", title, "TODO");
+#[derive(Debug)]
+enum Step {
+    SwapPos(usize, usize),
+    SwapChar(char, char),
+    RotateL(usize),
+    RotateR(usize),
+    RotateChar(char),
+    Reverse(usize, usize),
+    Move(usize, usize),
 }
 
-const INPUT_DEMO: &str = "";
+peg::parser! {
+    grammar input_parser() for str {
+        rule num() -> usize
+            = n:$(['0'..='9']+) {? n.parse().or(Err("number")) }
+
+        rule letter() -> char
+            = ['a'..='z']
+
+        rule step() -> Step
+            = "swap position " x:num() " with position " y:num() "\n" { Step::SwapPos(x, y) }
+            / "swap letter " x:letter() " with letter " y:letter() "\n" { Step::SwapChar(x, y) }
+            / "rotate left " x:num() (" steps\n" / " step\n") { Step::RotateL(x) }
+            / "rotate right " x:num() (" steps\n" / " step\n") { Step::RotateR(x) }
+            / "rotate based on position of letter " x:letter() "\n" { Step::RotateChar(x) }
+            / "reverse positions " x:num() " through " y:num() "\n" { Step::Reverse(x, y) }
+            / "move position " x:num() " to position " y:num() "\n" { Step::Move(x, y) }
+
+        pub rule file() -> Vec<Step>
+            = step()+
+    }
+}
+
+fn run(title: &str, input: &str, msg: &str) {
+    let data = input_parser::file(input).unwrap();
+    println!("{:?}", data);
+
+    let mut msg = msg.chars().collect_vec();
+    let msg = msg.as_mut_slice();
+
+    for rule in &data {
+        match rule {
+            &Step::SwapPos(x, y) => (msg[x], msg[y]) = (msg[y], msg[x]),
+            &Step::SwapChar(x, y) => msg.iter_mut().for_each(|c| if *c == x { *c = y } else if *c == y { *c = x }),
+            &Step::RotateL(x) => msg.rotate_left(x % msg.len()),
+            &Step::RotateR(x) => msg.rotate_right(x % msg.len()),
+            &Step::RotateChar(x) => {
+                let idx = msg.iter().find_position(|c| **c == x).unwrap().0;
+                // println!("- {} {}", idx, (idx + 1 + if idx >= 4 { 1 } else { 0 }));
+                msg.rotate_right((idx + 1 + if idx >= 4 { 1 } else { 0 }) % msg.len());
+            },
+            &Step::Reverse(x, y) => msg[x..=y].reverse(),
+            &Step::Move(x, y) => if x < y { msg[x..=y].rotate_left(1) } else { msg[y..=x].rotate_right(1) },
+        }
+        // println!("{}", msg.iter().collect::<String>());
+    }
+
+    println!("{} part 1: {}", title, msg.iter().collect::<String>());
+}
+
+fn run2(title: &str, input: &str, msg: &str) {
+    let data = input_parser::file(input).unwrap();
+
+    let mut msg = msg.chars().collect_vec();
+    let msg = msg.as_mut_slice();
+
+    for rule in data.iter().rev() {
+        match rule {
+            &Step::SwapPos(x, y) => (msg[x], msg[y]) = (msg[y], msg[x]),
+            &Step::SwapChar(x, y) => msg.iter_mut().for_each(|c| if *c == x { *c = y } else if *c == y { *c = x }),
+            &Step::RotateL(x) => msg.rotate_right(x % msg.len()),
+            &Step::RotateR(x) => msg.rotate_left(x % msg.len()),
+            &Step::RotateChar(x) => {
+                for i in 0.. {
+                    let mut msg2 = msg.to_vec();
+                    msg2.rotate_left(i);
+
+                    let idx = msg2.iter().find_position(|c| **c == x).unwrap().0;
+                    msg2.rotate_right((idx + 1 + if idx >= 4 { 1 } else { 0 }) % msg.len());
+
+                    if msg2 == msg {
+                        msg.rotate_left(i);
+                        break;
+                    }
+
+                }
+            },
+            &Step::Reverse(x, y) => msg[x..=y].reverse(),
+            &Step::Move(x, y) => if x < y { msg[x..=y].rotate_right(1) } else { msg[y..=x].rotate_left(1) },
+        }
+        // println!("{}", msg.iter().collect::<String>());
+    }
+
+    println!("{} part 2: {}", title, msg.iter().collect::<String>());
+}
+
+
+const INPUT_DEMO: &str = "swap position 4 with position 0
+swap letter d with letter b
+reverse positions 0 through 4
+rotate left 1 steps
+move position 1 to position 4
+move position 3 to position 0
+rotate based on position of letter b
+rotate based on position of letter d
+";
 
 fn main() {
-    run("demo", INPUT_DEMO);
-    run("input", &std::fs::read_to_string("21/input.txt").unwrap());
+    run("demo", INPUT_DEMO, "abcde");
+    run("input", &std::fs::read_to_string("21/input.txt").unwrap(), "abcdefgh");
+    run2("input", &std::fs::read_to_string("21/input.txt").unwrap(), "fbgdceah");
 }
--- 2016/22/src/main.rs
+++ 2016/22/src/main.rs
@@ -1,21 +1,283 @@
+use std::collections::{HashMap, HashSet, VecDeque};
+
+use itertools::Itertools;
+
+#[derive(Debug, Clone, Copy, PartialEq, Eq, Hash)]
+struct Disk {
+    size: u16,
+    used: u16,
+}
+
+#[derive(Debug, Clone, PartialEq, Eq, Hash)]
+struct State {
+    goal: (i32, i32),
+    disks: Vec<Disk>,
+}
+
+fn print(state: &State) {
+    let w = 38;
+    let h = 26;
+
+    println!("---");
+    for y in 0..h {
+        for x in 0..w {
+            let disk = state.disks[x + y*w];
+            assert!(disk.used <= disk.size);
+            if state.goal == (x as i32, y as i32) {
+                print!("G");
+            } else if disk.used == 0 {
+                print!("_");
+            } else if disk.size > 500 && disk.size - disk.used < 50 {
+                print!("#");
+            } else if disk.used > 50 {
+                print!(".");
+            } else {
+                print!("?");
+            }
+        }
+        println!();
+    }
+}
+
+fn mv(state: &State, src: (i32, i32), dst: (i32, i32)) -> State {
+    let w = 38;
+    let h = 26;
+
+    assert!((src.0 - dst.0).abs() + (src.1 - dst.1).abs() == 1);
+
+    let srcd = state.disks[(src.0 + src.1 * w) as usize];
+    let dstd = state.disks[(dst.0 + dst.1 * w) as usize];
+    assert!(srcd.used > 0);
+    assert!(dstd.size - dstd.used >= srcd.used);
+
+    let mut new = state.clone();
+    new.disks[(src.0 + src.1 * w) as usize].used = 0;
+    new.disks[(dst.0 + dst.1 * w) as usize].used += srcd.used;
+    if src == state.goal {
+        new.goal = dst;
+    }
+
+    new
+}
+
 fn run(title: &str, input: &str) {
-    let data: Vec<Vec<i32>> = input
+    let data: HashMap<(i32, i32), Disk> = input
         .lines()
+        .skip(2)
         .map(|line| {
-            line.split_ascii_whitespace()
-                .map(|n| str::parse(n).unwrap())
-                .collect()
+            let ts = line.split_whitespace().collect_vec();
+            let ns = ts[0].split("-").collect_vec();
+            let x = ns[1].strip_prefix("x").unwrap().parse().unwrap();
+            let y = ns[2].strip_prefix("y").unwrap().parse().unwrap();
+            let mut size = ts[1].strip_suffix("T").unwrap().parse().unwrap();
+            let mut used = ts[2].strip_suffix("T").unwrap().parse().unwrap();
+
+            // if size > 500 {
+            //     size = 500;
+            //     used = 500;
+            // } else if used > 0 {
+            //     size = 100;
+            //     used = 60;
+            // } else {
+            //     size = 100;
+            //     used = 0;
+            // }
+
+            ((x, y), Disk { size, used })
         })
         .collect();
 
-    println!("{} part 1: {}", title, "TODO");
+    // println!("{:?}", data);
 
-    println!("{} part 2: {}", title, "TODO");
+    let viable = data.values().permutations(2).filter(|disks| {
+        disks[0].used != 0 && (disks[1].size - disks[1].used) >= disks[0].used
+    }).count();
+
+    println!("{} part 1: {}", title, viable);
+
+    let (maxx, maxy) = data.keys().max().unwrap();
+    let (w, h) = (maxx + 1, maxy + 1);
+
+    println!("{} {}", w, h);
+
+    // for y in 0..h {
+    //     for x in 0..w {
+    //         let disk = data[&(x, y)];
+    //         if disk.used == 0 {
+    //             print!("_");
+    //         } else if disk.size > 500 && disk.size - disk.used < 50 {
+    //             print!("#");
+    //         } else if disk.used > 50 {
+    //             print!(".");
+    //         } else {
+    //             print!("?");
+    //         }
+    //     }
+    //     println!();
+    // }
+
+    let mut open = VecDeque::new();
+    let initial = State { goal: (w - 1, 0), disks: (0..h).map(|y| (0..w).map(|x| data[&(x, y)]).collect()).concat() };
+
+    let mut state = initial;
+    print(&state);
+    let mut steps = 0;
+    for i in 0..11 {
+        state = mv(&state, (15-i, 23), (15-i+1, 23));
+        steps += 1;
+    }
+    print(&state);
+    for i in 0..22 {
+        state = mv(&state, (5, 23-i-1), (5, 23-i));
+        steps += 1;
+    }
+    print(&state);
+    for i in 0..31 {
+        state = mv(&state, (5+i+1, 1), (5+i, 1));
+        steps += 1;
+    }
+    print(&state);
+    state = mv(&state, (36, 0), (36, 1));
+    state = mv(&state, (37, 0), (36, 0));
+    steps += 2;
+    for i in 0..36 {
+        let gx = state.goal.0;
+        state = mv(&state, (gx+1, 1), (gx+1, 0));
+        state = mv(&state, (gx, 1), (gx+1, 1));
+        state = mv(&state, (gx-1, 1), (gx, 1));
+        state = mv(&state, (gx-1, 0), (gx-1, 1));
+        state = mv(&state, (gx, 0), (gx-1, 0));
+        steps += 5;
+    }
+    print(&state);
+
+    println!("{}", steps);
+
+    return;
+
+
+
+    open.push_back((initial, 0));
+
+    let mut closed = HashSet::new();
+
+    while let Some((node, steps)) = open.pop_front() {
+        if node.goal == (0, 0) {
+            println!("{} part 2: {}", title, steps);
+            break;
+        }
+
+        // if steps < 2 {
+        //     println!("{}", steps);
+        //     for y in 0..h {
+        //         for x in 0..w {
+        //             let disk = data[&(x, y)];
+        //             if disk.used == 0 {
+        //                 print!("_");
+        //             } else if disk.size > 500 && disk.size - disk.used < 50 {
+        //                 print!("#");
+        //             } else if disk.used > 50 {
+        //                 print!(".");
+        //             } else {
+        //                 print!("?");
+        //             }
+        //         }
+        //         println!();
+        //     }
+        // } else {
+        //     break;
+        // }
+
+        if open.len() % 10_000 == 0 || closed.len() % 10_000 == 0 {
+            println!("{} {} {}...", steps, open.len(), closed.len());
+        }
+
+        // if steps == 1 && node.disks[(1 + 0*w) as usize].used != 0 {
+        //     continue;
+        // }
+        // if steps == 2 && node.goal != (1, 0) {
+        //     continue;
+        // }
+        // if steps == 3 && node.disks[(2 + 1*w) as usize].used != 0 {
+        //     continue;
+        // }
+        // if steps == 4 && node.disks[(1 + 1*w) as usize].used != 0 {
+        //     continue;
+        // }
+        // if steps == 5 && node.disks[(0 + 1*w) as usize].used != 0 {
+        //     continue;
+        // }
+        // if steps == 6 && node.disks[(0 + 0*w) as usize].used != 0 {
+        //     continue;
+        // }
+        // if steps == 7 && node.disks[(1 + 0*w) as usize].used != 0 {
+        //     continue;
+        // }
+
+        // println!("{} {:?}", steps, node);
+
+        // if steps > 10 {
+        //     break;
+        // }
+
+        // if !closed.insert(node.clone()) {
+        //     continue;
+        // }
+
+        // println!(">>>>");
+        for y in 0..h {
+            for x in 0..w {
+                // let range = 10;
+                // if (x - node.goal.0).abs() > range || (y - node.goal.1).abs() > range {
+                //     continue;
+                // }
+
+                for n in [(x-1, y), (x+1, y), (x, y-1), (x, y+1)] {
+                    if n.0 >= 0 && n.0 < w && n.1 >= 0 && n.1 < h {
+                        let src = node.disks[(x + y * w) as usize];
+                        let dst = node.disks[(n.0 + n.1 * w) as usize];
+                        if src.used > 0 && dst.size - dst.used >= src.used {
+                            let mut new = node.clone();
+                            new.disks[(x + y * w) as usize].used = 0;
+                            new.disks[(n.0 + n.1 * w) as usize].used += src.used;
+                            if new.goal == (x, y) {
+                                // println!("!! {:?} {:?} {:?}", new.goal, n, new);
+                                new.goal = n;
+                            }
+                            // println!("{:?}", new);
+                            if closed.insert(new.clone()) {
+                                // println!("{:?}({}/{}) -> {:?}({}/{})", (x, y), src.used, src.size, n, dst.used, dst.size);
+                                open.push_back((new, steps + 1));
+                            }
+                        }
+                    }
+                }
+            }
+        }
+        // println!("<<<<");
+    }
+
+    // State:
+    //   Node containing G
+    //   Size, used in each cell
+    // Actions:
+    //   Move from A to neighbour
 }
 
-const INPUT_DEMO: &str = "";
+const INPUT_DEMO: &str = "
+Filesystem            Size  Used  Avail  Use%
+/dev/grid/node-x0-y0   10T    8T     2T   80%
+/dev/grid/node-x0-y1   11T    6T     5T   54%
+/dev/grid/node-x0-y2   32T   28T     4T   87%
+/dev/grid/node-x1-y0    9T    7T     2T   77%
+/dev/grid/node-x1-y1    8T    0T     8T    0%
+/dev/grid/node-x1-y2   11T    7T     4T   63%
+/dev/grid/node-x2-y0   10T    6T     4T   60%
+/dev/grid/node-x2-y1    9T    8T     1T   88%
+/dev/grid/node-x2-y2    9T    6T     3T   66%
+";
 
 fn main() {
-    run("demo", INPUT_DEMO);
+    // run("demo", INPUT_DEMO);
     run("input", &std::fs::read_to_string("22/input.txt").unwrap());
 }
