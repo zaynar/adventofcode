8d501b35c59290e486dc7a3e2d8ac0e2100886a8
Philip Taylor <philip@zaynar.co.uk>
2024-12-08 12:54:54 +0000

    2016 day 23-25

--- 2016/23/src/main.rs
+++ 2016/23/src/main.rs
@@ -1,21 +1,125 @@
+use std::collections::{HashMap, HashSet};
+
+use itertools::Itertools;
+
 fn run(title: &str, input: &str) {
-    let data: Vec<Vec<i32>> = input
-        .lines()
-        .map(|line| {
-            line.split_ascii_whitespace()
-                .map(|n| str::parse(n).unwrap())
-                .collect()
-        })
-        .collect();
+    let mut pc: i32 = 0;
+    let lines = input.lines().collect_vec();
+    let mut regs: HashMap<&str, i32> = HashMap::new();
+    regs.insert("a", 12);
+    regs.insert("b", 0);
+    regs.insert("c", 0);
+    regs.insert("d", 0);
 
-    println!("{} part 1: {}", title, "TODO");
+    let mut lines = lines.iter().map(|line| line.split_once(" ").unwrap()).collect_vec();
 
-    println!("{} part 2: {}", title, "TODO");
+    let mut toggled = HashSet::new();
+
+    let mut i = 0;
+
+    while pc < lines.len() as i32 {
+        let (op, r) = lines[pc as usize];
+
+        i += 1;
+        if i % 10_000_000 == 0 {
+            println!("{}: {} {:?} {:?} --- {:?} --- {:?}", i, pc, regs, lines[pc as usize], lines, toggled);
+        }
+
+        match op {
+            /*
+            Replace input.txt line 5-11 with
+
+            mad b d a
+            cpy 0 c
+            cpy 0 d
+            nop _
+            nop _
+            nop _
+             */
+            "add" => {
+                let (x, y) = r.split_once(" ").unwrap();
+                regs.insert(y, regs[y] + if regs.contains_key(x) { regs[x] } else { x.parse().unwrap() });
+                pc += 1;
+            }
+            "mad" => {
+                let (x, y) = r.split_once(" ").unwrap();
+                regs.insert("a", regs["b"] * regs["d"]);
+                pc += 1;
+            }
+            "nop" => {
+                pc += 1;
+            }
+
+            "cpy" => {
+                let (x, y) = r.split_once(" ").unwrap();
+                if regs.contains_key(y) {
+                    regs.insert(y, if regs.contains_key(x) { regs[x] } else { x.parse().unwrap() });
+                }
+                pc += 1;
+            }
+            "inc" => {
+                if regs.contains_key(r) {
+                    regs.insert(r, regs[r] + 1);
+                }
+                pc += 1;
+            }
+            "dec" => {
+                if regs.contains_key(r) {
+                    regs.insert(r, regs[r] - 1);
+                }
+                pc += 1;
+            }
+            "jnz" => {
+                let (x, y) = r.split_once(" ").unwrap();
+                let x = if regs.contains_key(x) { regs[x] } else { x.parse().unwrap() };
+                let y = if regs.contains_key(y) { regs[y] } else { y.parse().unwrap() };
+                if x != 0 {
+                    pc += y;
+                } else {
+                    pc += 1;
+                }
+            }
+            "tgl" => {
+                let x = r;
+                let x = if regs.contains_key(x) { regs[x] } else { x.parse().unwrap() };
+
+                if pc + x >= 0 && pc + x < lines.len() as i32 {
+                    if toggled.insert(pc + x) {
+                        println!("toggle {:?}", toggled);
+                    }
+
+                    lines[(pc + x) as usize].0 = match lines[(pc + x) as usize].0 {
+                        "cpy" => "jnz",
+                        "inc" => "dec",
+                        "dec" => "inc",
+                        "jnz" => "cpy",
+                        "tgl" => "inc",
+                        _ => panic!(),
+                    };
+                }
+
+                pc += 1;
+            }
+            _ => panic!()
+        }
+    }
+
+    println!("{:?}", regs);
+
+    println!("{} part N: {}", title, regs["a"]);
 }
 
-const INPUT_DEMO: &str = "";
+const INPUT_DEMO: &str = "cpy 2 a
+tgl a
+tgl a
+tgl a
+cpy 1 a
+dec a
+dec a
+";
 
 fn main() {
     run("demo", INPUT_DEMO);
-    run("input", &std::fs::read_to_string("23/input.txt").unwrap());
+    // run("input", &std::fs::read_to_string("23/input.txt").unwrap());
+    run("input", &std::fs::read_to_string("23/input-mod.txt").unwrap());
 }
--- 2016/24/src/main.rs
+++ 2016/24/src/main.rs
@@ -1,19 +1,121 @@
-fn run(title: &str, input: &str) {
-    let data: Vec<Vec<i32>> = input
-        .lines()
-        .map(|line| {
-            line.split_ascii_whitespace()
-                .map(|n| str::parse(n).unwrap())
-                .collect()
-        })
-        .collect();
+use std::collections::{HashMap, HashSet, VecDeque};
 
-    println!("{} part 1: {}", title, "TODO");
+use itertools::Itertools;
 
-    println!("{} part 2: {}", title, "TODO");
+fn flood(grid: &Vec<Vec<char>>, x0: i32, y0: i32) -> Vec<(i32, i32)> {
+    let mut seen = HashSet::new();
+    let mut open = VecDeque::new();
+    open.push_back((x0, y0, 0));
+    seen.insert((x0, y0));
+
+    let mut ret = Vec::new();
+
+    while let Some((x, y, d)) = open.pop_front() {
+        for (dx, dy) in [(-1, 0), (1, 0), (0, -1), (0, 1)] {
+            if seen.insert((x + dx, y + dy)) {
+                match grid[(y + dy) as usize][(x + dx) as usize] {
+                    c @ ('0'..='9') => {
+                        ret.push((c.to_digit(10).unwrap() as i32, d + 1));
+                        open.push_back((x + dx, y + dy, d + 1));
+                    }
+                    '.' => {
+                        open.push_back((x + dx, y + dy, d + 1));
+                    }
+                    '#' => (),
+                    c @ _ => panic!("Invalid char {}", c),
+                }
+            }
+        }
+    }
+
+    ret
 }
 
-const INPUT_DEMO: &str = "";
+// #[derive(PartialEq, Eq, Clone, Hash, Debug)]
+// struct State2 {
+//     pos: [char; 4],
+//     keys: Vec<char>,
+//     steps: i32,
+// }
+
+// impl PartialOrd for State2 {
+//     fn partial_cmp(&self, other: &Self) -> Option<std::cmp::Ordering> {
+//         Some(self.cmp(other))
+//     }
+// }
+
+// impl Ord for State2 {
+//     fn cmp(&self, other: &Self) -> std::cmp::Ordering {
+//         other
+//             .steps
+//             .cmp(&self.steps)
+//             .then(self.keys.cmp(&other.keys))
+//             .then(self.pos.cmp(&other.pos))
+//     }
+// }
+
+fn run(title: &str, input: &str) {
+    let grid: Vec<Vec<char>> = input.lines().map(|line| line.chars().collect()).collect();
+
+    let w = grid[0].len();
+    let h = grid.len();
+
+    let mut edges = HashMap::new();
+
+    /*
+
+    Calculate matrix of shortest paths between all nodes (@, a, A), treating all aA as walls
+
+    Starting at @, BFS the graph (filtering out doors we don't have keys for), until got all keys
+
+     */
+
+    for y in 0..h {
+        for x in 0..w {
+            match grid[y][x] {
+                src @ ('0'..='9') => {
+                    for (dst, d) in flood(&grid, x as i32, y as i32) {
+                        edges
+                            .entry(src.to_digit(10).unwrap() as i32)
+                            .or_insert_with(|| HashSet::new())
+                            .insert((dst, d));
+                    }
+                }
+                _ => (),
+            }
+        }
+    }
+
+    let num_keys = edges.keys().count();
+
+    println!("{:?}", edges);
+    println!("{} keys", num_keys);
+
+    let mut shortest = i32::MAX;
+    for p in (1..num_keys as i32).permutations(num_keys - 1) {
+        let mut prev = 0;
+        let mut dist = 0;
+        for n in &p {
+            dist += edges[&prev].iter().find_map(|e| if e.0 == *n { Some(e.1) } else { None }).unwrap();
+            prev = *n;
+        }
+
+        // Part 2:
+        dist += edges[&prev].iter().find_map(|e| if e.0 == 0 { Some(e.1) } else { None }).unwrap();
+
+        // println!("{} {:?}", dist, p);
+        shortest = shortest.min(dist);
+    }
+
+    println!("{} part N: {}", title, shortest);
+}
+
+const INPUT_DEMO: &str = "###########
+#0.1.....2#
+#.#######.#
+#4.......3#
+###########
+";
 
 fn main() {
     run("demo", INPUT_DEMO);
