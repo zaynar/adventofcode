dd7f48451d68f527f58e523063e568dac7d72eae
Philip Taylor <philip@zaynar.co.uk>
2025-01-08 17:10:28 +0000

    2022 templates

---
+++ 2022/01/src/main.rs
@@ -0,0 +1,21 @@
+fn run(title: &str, input: &str) {
+    let data: Vec<Vec<i32>> = input
+        .lines()
+        .map(|line| {
+            line.split_ascii_whitespace()
+                .map(|n| str::parse(n).unwrap())
+                .collect()
+        })
+        .collect();
+
+    println!("{} part 1: {}", title, "TODO");
+
+    println!("{} part 2: {}", title, "TODO");
+}
+
+const INPUT_DEMO: &str = "";
+
+fn main() {
+    run("demo", INPUT_DEMO);
+    run("input", &std::fs::read_to_string("01/input.txt").unwrap());
+}
