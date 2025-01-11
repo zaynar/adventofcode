d897146e1b033fbf9970fa57faa888dd391c9080
Philip Taylor <philip@zaynar.co.uk>
2025-01-11 20:32:25 +0000

    2022 day 21-22

--- 2022/21/src/main.rs
+++ 2022/21/src/main.rs
@@ -1,19 +1,211 @@
-fn run(title: &str, input: &str) {
-    let data: Vec<Vec<i32>> = input
-        .lines()
-        .map(|line| {
-            line.split_ascii_whitespace()
-                .map(|n| str::parse(n).unwrap())
-                .collect()
-        })
-        .collect();
+// Part 1: 9 mins
+// Part 1+2: 33 mins
 
-    println!("{} part 1: {}", title, "TODO");
+use std::collections::HashMap;
 
-    println!("{} part 2: {}", title, "TODO");
+#[derive(Debug)]
+enum Op {
+    Num(i64),
+    Add(String, String),
+    Sub(String, String),
+    Mul(String, String),
+    Div(String, String),
 }
 
-const INPUT_DEMO: &str = "";
+#[derive(Debug)]
+enum Node {
+    Humn,
+    Num(i64),
+    Add(Box<Node>, Box<Node>),
+    Sub(Box<Node>, Box<Node>),
+    Mul(Box<Node>, Box<Node>),
+    Div(Box<Node>, Box<Node>),
+}
+
+fn eval(data: &HashMap<String, Op>, id: &str, cache: &mut HashMap<String, i64>) -> i64 {
+    // if let Some(n) = cache.get(id) {
+    //     return *n;
+    // }
+
+    let n: i64 = match &data[id] {
+        Op::Num(n) => *n,
+        Op::Add(a, b) => eval(data, &a, cache) + eval(data, &b, cache),
+        Op::Sub(a, b) => eval(data, &a, cache) - eval(data, &b, cache),
+        Op::Mul(a, b) => eval(data, &a, cache) * eval(data, &b, cache),
+        Op::Div(a, b) => eval(data, &a, cache) / eval(data, &b, cache),
+    };
+
+    cache.insert(id.to_owned(), n);
+
+    n
+}
+
+fn expand(data: &HashMap<String, Op>, id: &str) -> Node {
+    if id == "humn" {
+        return Node::Humn;
+    }
+
+    match &data[id] {
+        Op::Num(n) => Node::Num(*n),
+        Op::Add(a, b) => Node::Add(Box::new(expand(data, a)), Box::new(expand(data, b))),
+        Op::Sub(a, b) => Node::Sub(Box::new(expand(data, a)), Box::new(expand(data, b))),
+        Op::Mul(a, b) => Node::Mul(Box::new(expand(data, a)), Box::new(expand(data, b))),
+        Op::Div(a, b) => Node::Div(Box::new(expand(data, a)), Box::new(expand(data, b))),
+    }
+}
+
+fn simplify(node: &Node) -> Node {
+    match node {
+        Node::Humn => Node::Humn,
+        Node::Num(n) => Node::Num(*n),
+        Node::Add(a, b) => {
+            let a = simplify(a);
+            let b = simplify(b);
+            match (&a, &b) {
+                (Node::Num(a), Node::Num(b)) => Node::Num(a + b),
+                _ => Node::Add(Box::new(a), Box::new(b))
+            }
+        }
+        Node::Sub(a, b) => {
+            let a = simplify(a);
+            let b = simplify(b);
+            match (&a, &b) {
+                (Node::Num(a), Node::Num(b)) => Node::Num(a - b),
+                _ => Node::Sub(Box::new(a), Box::new(b))
+            }
+        }
+        Node::Mul(a, b) => {
+            let a = simplify(a);
+            let b = simplify(b);
+            match (&a, &b) {
+                (Node::Num(a), Node::Num(b)) => Node::Num(a * b),
+                _ => Node::Mul(Box::new(a), Box::new(b))
+            }
+        }
+        Node::Div(a, b) => {
+            let a = simplify(a);
+            let b = simplify(b);
+            match (&a, &b) {
+                (Node::Num(a), Node::Num(b)) => Node::Num(a / b),
+                _ => Node::Div(Box::new(a), Box::new(b))
+            }
+        }
+    }
+}
+
+fn equate(node: &Node, target: i64) -> i64 {
+    match node {
+        Node::Humn => return target,
+        Node::Num(n) => panic!(),
+        Node::Add(a, b) => {
+            // a + b == target
+            if let Node::Num(a) = **a {
+                return equate(b, target - a);
+            }
+            if let Node::Num(b) = **b {
+                return equate(a, target - b);
+            }
+        }
+        Node::Sub(a, b) => {
+            // a - b == target
+            if let Node::Num(a) = **a {
+                return equate(b, a - target);
+            }
+            if let Node::Num(b) = **b {
+                return equate(a, target + b);
+            }
+        }
+        Node::Mul(a, b) => {
+            // a * b == target
+            if let Node::Num(a) = **a {
+                return equate(b, target / a);
+            }
+            if let Node::Num(b) = **b {
+                return equate(a, target / b);
+            }
+        }
+        Node::Div(a, b) => {
+            // a / b == target
+            if let Node::Num(a) = **a {
+                return equate(b, a / target);
+            }
+            if let Node::Num(b) = **b {
+                return equate(a, b * target);
+            }
+        }
+    }
+
+    panic!();
+}
+
+fn run(title: &str, input: &str) {
+    let mut data: HashMap<String, Op> = HashMap::from_iter(
+        input.lines()
+        .map(|line| {
+            let (id, out) = line.split_once(": ").unwrap();
+            if let Ok(n) = out.parse::<i64>() {
+                (id.to_owned(), Op::Num(n))
+            } else {
+                let a = out[0..4].to_owned();
+                let b = out[7..11].to_owned();
+                (id.to_owned(), match &out[5..6] {
+                    "+" => Op::Add(a, b),
+                    "-" => Op::Sub(a, b),
+                    "*" => Op::Mul(a, b),
+                    "/" => Op::Div(a, b),
+                    _ => panic!()
+                })
+            }
+        })
+    );
+
+    // println!("{:?}", data);
+
+    // for (k, v) in &data {
+    //     println!("{};", k);
+    //     match v {
+    //         Op::Num(n) => (),
+    //         Op::Add(a, b) => println!("{} -> {}; {} -> {};", k, a, k, b),
+    //         Op::Sub(a, b) => println!("{} -> {}; {} -> {};", k, a, k, b),
+    //         Op::Mul(a, b) => println!("{} -> {}; {} -> {};", k, a, k, b),
+    //         Op::Div(a, b) => println!("{} -> {}; {} -> {};", k, a, k, b),
+    //     };
+    // }
+
+    let mut cache = HashMap::new();
+
+    println!("{} part 1: {}", title, eval(&data, "root", &mut cache));
+
+    match &data["root"] {
+        Op::Add(lhs, rhs) => {
+            let lhs = simplify(&expand(&data, lhs));
+            let rhs = simplify(&expand(&data, rhs));
+            println!("{:?}", lhs);
+            println!("{:?}", rhs);
+            if let Node::Num(rhs) = rhs {
+                println!("{} part 2: {}", title, equate(&lhs, rhs));
+            }
+        }
+        _ => panic!()
+    };
+}
+
+const INPUT_DEMO: &str = "root: pppw + sjmn
+dbpl: 5
+cczh: sllz + lgvd
+zczc: 2
+ptdq: humn - dvpt
+dvpt: 3
+lfqf: 4
+humn: 5
+ljgn: 2
+sjmn: drzm * dbpl
+sllz: 4
+pppw: cczh / lfqf
+lgvd: ljgn * ptdq
+drzm: hmdt - zczc
+hmdt: 32
+";
 
 fn main() {
     run("demo", INPUT_DEMO);
--- 2022/22/src/main.rs
+++ 2022/22/src/main.rs
@@ -1,21 +1,235 @@
-fn run(title: &str, input: &str) {
-    let data: Vec<Vec<i32>> = input
-        .lines()
-        .map(|line| {
-            line.split_ascii_whitespace()
-                .map(|n| str::parse(n).unwrap())
-                .collect()
-        })
-        .collect();
+// Part 1: 14 mins
+// Part 1+2: 50 mins
 
-    println!("{} part 1: {}", title, "TODO");
+use aocgrid::Grid;
+use itertools::Itertools;
 
-    println!("{} part 2: {}", title, "TODO");
+fn run(title: &str, input: &str, size: i32) {
+    let (grid, path) = input.split_once("\n\n").unwrap();
+    let mut grid = Grid::from(grid);
+    let path = path.split_inclusive(|c| c == 'L' || c == 'R').collect_vec();
+
+    let start = grid.find(&'.').unwrap();
+
+    let mut dir = (1, 0);
+    let mut pos = start;
+
+    for step in &path {
+        let n: u32 = step[0..step.len() - 1].parse().unwrap();
+        let turn = step.chars().last().unwrap();
+
+        for i in 0..n {
+            let mut np = (pos.0 + dir.0, pos.1 + dir.1);
+            let mut next = grid.try_get(np.0, np.1);
+            if next.is_none() || next == Some(&' ') {
+                np.0 -= dir.0 * 200;
+                np.1 -= dir.1 * 200;
+            }
+            next = grid.try_get(np.0, np.1);
+            while next.is_none() || next == Some(&' ') {
+                np.0 += dir.0;
+                np.1 += dir.1;
+                next = grid.try_get(np.0, np.1);
+            }
+
+            if next == Some(&'#') {
+                break;
+            }
+
+            pos = np;
+
+            grid.set(pos.0, pos.1, '*');
+        }
+
+        match turn {
+            'R' => dir = (-dir.1, dir.0),
+            'L' => dir = (dir.1, -dir.0),
+            '\n' => (),
+            _ => panic!()
+        };
+    }
+
+    // println!("{}", grid);
+    // println!("{:?}", (pos, dir));
+
+    let score = (pos.1 + 1) * 1000 + (pos.0 + 1) * 4 + match dir {
+        (1, 0) => 0,
+        (0, 1) => 1,
+        (-1, 0) => 2,
+        (0, -1) => 3,
+        _ => panic!()
+    };
+
+    println!("{} part 1: {}", title, score);
+
+
+    let mut dir = (1, 0);
+    let mut pos = start;
+
+    for step in &path {
+        let n: u32 = step[0..step.len() - 1].parse().unwrap();
+        let turn = step.chars().last().unwrap();
+
+        for i in 0..n {
+            let mut np = (pos.0 + dir.0, pos.1 + dir.1);
+            let mut nd = dir;
+            if region(np, size) == 0 {
+                let rx = pos.0 % size;
+                let ry = pos.1 % size;
+                match (region(pos, size), dir) {
+                    // (1, (-1, 0)) => panic!(),
+                    // (1, (1, 0)) => panic!(),
+                    // (1, (0, -1)) => panic!(),
+                    // (2, (-1, 0)) => panic!(),
+                    // (2, (0, -1)) => panic!(),
+                    // (2, (0, 1)) => panic!(),
+                    // (3, (0, -1)) => panic!(),
+                    // (3, (0, 1)) => panic!(),
+                    // (4, (1, 0)) => panic!(),
+                    // (5, (-1, 0)) => panic!(),
+                    // (5, (0, 1)) => panic!(),
+                    // (6, (0, -1)) => panic!(),
+                    // (6, (1, 0)) => panic!(),
+                    // (6, (0, 1)) => panic!(),
+
+                    (1, (-1, 0)) => {
+                        np = (0, size*3 - ry - 1);
+                        nd = (1, 0);
+                    },
+                    (1, (0, -1)) => { // 1 -> 6
+                        np = (0, size*3 + rx);
+                        nd = (1, 0);
+                    },
+                    (2, (0, -1)) => { // 2 -> 6
+                        np = (rx, size*4- 1);
+                        nd = (0, -1);
+                    },
+                    (2, (0, 1)) => { // 2 -> 3
+                        np = (size*2-1, size + rx);
+                        nd = (-1, 0);
+                    },
+                    (2, (1, 0)) => { // 2 -> 5
+                        np = (size*2-1, size*3-1 - ry);
+                        nd = (-1, 0);
+                    },
+                    (3, (-1, 0)) => { // 3 -> 4
+                        np = (ry, size*2);
+                        nd = (0, 1);
+                    },
+                    (3, (1, 0)) => { // 3 -> 2
+                        np = (2*size + ry, size-1);
+                        nd = (0, -1);
+                    },
+                    (4, (0, -1)) => { // 4 -> 3
+                        np = (size, size + rx);
+                        nd = (1, 0);
+                    },
+                    (4, (-1, 0)) => {
+                        np = (size, size - ry - 1);
+                        nd = (1, 0);
+                    },
+                    (5, (0, 1)) => {
+                        np = (size - 1, size*3 + rx);
+                        nd = (-1, 0);
+                    },
+                    (5, (1, 0)) => { // 5 -> 2
+                        np = (size*3 - 1, size-1 - ry);
+                        nd = (-1, 0);
+                    },
+                    (6, (-1, 0)) => { // 6 -> 1
+                        np = (size + ry, 0);
+                        nd = (0, 1);
+                    },
+                    (6, (1, 0)) => { // 6 -> 5
+                        np = (size + ry, size*3-1);
+                        nd = (0, -1);
+                    },
+                    (6, (0, 1)) => { // 6 -> 2
+                        np = (size + rx, 0);
+                        nd = (0, 1);
+                    },
+                    _ => panic!("not handled {:?} {:?} @ {:?}", region(pos, size), dir, pos),
+                }
+            }
+
+            println!("{:?}/{:?} {:?}/{:?} -- {:?}", pos, dir, np, nd, (region(pos, size), dir));
+
+            assert_ne!(region(np, size), 0);
+            if grid.get(np.0, np.1) == &'#' {
+                break;
+            }
+
+            pos = np;
+            dir = nd;
+
+            grid.set(pos.0, pos.1, '*');
+        }
+
+        match turn {
+            'R' => dir = (-dir.1, dir.0),
+            'L' => dir = (dir.1, -dir.0),
+            '\n' => (),
+            _ => panic!()
+        };
+    }
+
+    // println!("{}", grid);
+    // println!("{:?}", (pos, dir));
+
+    let score = (pos.1 + 1) * 1000 + (pos.0 + 1) * 4 + match dir {
+        (1, 0) => 0,
+        (0, 1) => 1,
+        (-1, 0) => 2,
+        (0, -1) => 3,
+        _ => panic!()
+    };
+
+    println!("{} part 2: {}", title, score);
 }
 
-const INPUT_DEMO: &str = "";
+// fn region((x, y): (i32, i32), size: i32) -> i32 {
+//     match (x / size, y / size) {
+//         (2, 0) => 1,
+//         (0, 1) => 2,
+//         (1, 1) => 3,
+//         (2, 1) => 4,
+//         (2, 2) => 5,
+//         (3, 2) => 6,
+//         _ => 0,
+//     }
+// }
+fn region((x, y): (i32, i32), size: i32) -> i32 {
+    if x < 0 || y < 0 {
+        return 0;
+    }
+    match (x / size, y / size) {
+        (1, 0) => 1,
+        (2, 0) => 2,
+        (1, 1) => 3,
+        (0, 2) => 4,
+        (1, 2) => 5,
+        (0, 3) => 6,
+        _ => 0,
+    }
+}
+
+const INPUT_DEMO: &str = "        ...#
+        .#..
+        #...
+        ....
+...#.......#
+........#...
+..#....#....
+..........#.
+        ...#....
+        .....#..
+        .#......
+        ......#.
+
+10R5L5R10L4R5L5
+";
 
 fn main() {
-    run("demo", INPUT_DEMO);
-    run("input", &std::fs::read_to_string("22/input.txt").unwrap());
+    // run("demo", INPUT_DEMO, 4);
+    run("input", &std::fs::read_to_string("22/input.txt").unwrap(), 50);
 }
