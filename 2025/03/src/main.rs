59449bef9b23e2e7102672f3c8bc91ae14d2cef2
Philip Taylor <philip@zaynar.co.uk>
2025-12-03 10:13:23 +0000

    2025 day 3

--- 2025/03/src/main.rs
+++ 2025/03/src/main.rs
@@ -1,19 +1,89 @@
-fn run(title: &str, input: &str) {
-    let data: Vec<Vec<i32>> = input
-        .lines()
-        .map(|line| {
-            line.split_ascii_whitespace()
-                .map(|n| str::parse(n).unwrap())
-                .collect()
-        })
-        .collect();
+// Part 1: 7 mins
+// Part 1+2: 21 mins
 
-    println!("{} part 1: {}", title, "TODO");
+use std::collections::HashMap;
 
-    println!("{} part 2: {}", title, "TODO");
+fn max_jolt(buf: &[u32], n: u32, digs: usize) -> u32 {
+    if digs == 0 {
+        return n;
+    }
+
+    if buf.is_empty() {
+        return 0;
+    }
+
+    buf.iter()
+        .enumerate()
+        .map(|(i, c)| max_jolt(&buf[i + 1..], n * 10 + c, digs - 1))
+        .max()
+        .unwrap()
 }
 
-const INPUT_DEMO: &str = "";
+fn max_jolt2(
+    cache: &mut HashMap<(usize, usize), Option<u64>>,
+    buf: &[u32],
+    off: usize,
+    digs: usize,
+) -> Option<u64> {
+    if digs == 0 {
+        return Some(0);
+    }
+
+    if off == buf.len() {
+        return None;
+    }
+
+    let key = (off, digs);
+    if let Some(r) = cache.get(&key) {
+        *r
+    } else {
+        let r = buf
+            .iter()
+            .enumerate()
+            .skip(off)
+            .filter_map(|(i, c)| {
+                max_jolt2(cache, buf, i + 1, digs - 1)
+                    .map(|n| n + *c as u64 * 10u64.pow((digs - 1) as u32))
+            })
+            .max();
+        cache.insert(key, r);
+        r
+    }
+}
+
+fn run(title: &str, input: &str) {
+    let data: Vec<Vec<u32>> = input
+        .lines()
+        .map(|line| line.chars().map(|n| n.to_digit(10).unwrap()).collect())
+        .collect();
+
+    let mut part1 = 0;
+    let mut part2 = 0;
+
+    for bank in &data {
+        let mut cache = HashMap::new();
+
+        // println!(
+        //     "{:?} {} {:?}",
+        //     bank,
+        //     max_jolt(bank, 0, 2),
+        //     max_jolt2(&mut cache, bank, 0, 12)
+        // );
+
+        part1 += max_jolt(bank, 0, 2);
+
+        part2 += max_jolt2(&mut cache, bank, 0, 12).unwrap();
+    }
+
+    println!("{} part 1: {}", title, part1);
+    println!("{} part 2: {}", title, part2);
+}
+
+const INPUT_DEMO: &str = "987654321111111
+811111111111119
+234234234234278
+818181911112111
+";
 
 fn main() {
     run("demo", INPUT_DEMO);
