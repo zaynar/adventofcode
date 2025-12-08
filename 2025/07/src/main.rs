532d116c4fdd70c915e858478572cfabffe37bec
Philip Taylor <philip@zaynar.co.uk>
2025-12-08 09:28:59 +0000

    2025 day 7

--- 2025/07/src/main.rs
+++ 2025/07/src/main.rs
@@ -1,19 +1,79 @@
+// Part 1: 6 mins
+// Part 1+2: 10 mins
+
+use std::collections::HashMap;
+
+use aocgrid::Grid;
+
 fn run(title: &str, input: &str) {
-    let data: Vec<Vec<i32>> = input
-        .lines()
-        .map(|line| {
-            line.split_ascii_whitespace()
-                .map(|n| str::parse(n).unwrap())
-                .collect()
-        })
-        .collect();
+    let grid = Grid::from(input);
 
-    println!("{} part 1: {}", title, "TODO");
+    let start = grid.find(&'S').unwrap();
 
-    println!("{} part 2: {}", title, "TODO");
+    let mut part1 = 0;
+
+    // let mut beams = vec![start.0];
+    // for y in 1..grid.height() {
+    //     let mut newbeams = vec![];
+    //     for &x in &beams {
+    //         if *grid.get(x, y) == '.' {
+    //             newbeams.push(x);
+    //         } else if *grid.get(x, y) == '^' {
+    //             newbeams.push(x-1);
+    //             newbeams.push(x+1);
+    //             part1 += 1;
+    //         } else {
+    //             panic!();
+    //         }
+    //     }
+    //     newbeams.sort();
+    //     newbeams.dedup();
+    //     beams = newbeams;
+    // }
+
+    let mut beams = HashMap::new();
+    beams.insert(start.0, 1);
+
+    for y in 1..grid.height() {
+        let mut newbeams = HashMap::new();
+        for (&x, &n) in &beams {
+            if *grid.get(x, y) == '.' {
+                *newbeams.entry(x).or_default() += n;
+            } else if *grid.get(x, y) == '^' {
+                *newbeams.entry(x-1).or_default() += n;
+                *newbeams.entry(x+1).or_default() += n;
+                part1 += 1;
+            } else {
+                panic!();
+            }
+        }
+        beams = newbeams;
+    }
+
+    let part2 = beams.values().sum::<u64>();
+
+    println!("{} part 1: {}", title, part1);
+
+    println!("{} part 2: {}", title, part2);
 }
 
-const INPUT_DEMO: &str = "";
+const INPUT_DEMO: &str = ".......S.......
+...............
+.......^.......
+...............
+......^.^......
+...............
+.....^.^.^.....
+...............
+....^.^...^....
+...............
+...^.^...^.^...
+...............
+..^...^.....^..
+...............
+.^.^.^.^.^...^.
+...............
+";
 
 fn main() {
     run("demo", INPUT_DEMO);
