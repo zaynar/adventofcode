21a9050ac6e072c061cefe880c16b61efba7a1ba
Philip Taylor <philip@zaynar.co.uk>
2025-12-08 09:28:52 +0000

    2025 day 6

--- 2025/06/src/main.rs
+++ 2025/06/src/main.rs
@@ -1,19 +1,70 @@
+// Part 1: 4 mins
+// Part 1+2: 20 mins
+
+use itertools::Itertools;
+
 fn run(title: &str, input: &str) {
-    let data: Vec<Vec<i32>> = input
-        .lines()
+    let mut lines = input.lines().collect_vec();
+    let ops = lines.pop().unwrap().split_ascii_whitespace().collect_vec();
+    let nums = lines
+        .iter()
         .map(|line| {
             line.split_ascii_whitespace()
-                .map(|n| str::parse(n).unwrap())
-                .collect()
+                .map(|n| n.parse::<u64>().unwrap())
+                .collect_vec()
         })
-        .collect();
+        .collect_vec();
 
-    println!("{} part 1: {}", title, "TODO");
+    let mut part1 = 0;
 
-    println!("{} part 2: {}", title, "TODO");
+    for (i, op) in ops.iter().enumerate() {
+        part1 += match *op {
+            "*" => nums.iter().map(|n| n[i]).product::<u64>(),
+            "+" => nums.iter().map(|n| n[i]).sum::<u64>(),
+            _ => panic!(),
+        }
+    }
+
+    println!("{} part 1: {}", title, part1);
+
+    let mut part2 = 0;
+
+    let mut lines = input.lines().collect_vec();
+    let ops = lines.pop().unwrap().to_owned() + "         #";
+    let opind = ops.char_indices().filter(|(i, op)| *op != ' ');
+    let linechars = lines
+        .iter()
+        .map(|line| line.chars().collect_vec())
+        .collect_vec();
+    for ((i0, op0), (i1, op1)) in opind.tuple_windows() {
+        // println!("{i0} {op0} - {i1}");
+        let mut ns = vec![];
+        for i in (i0..i1 - 1).rev() {
+            let n = linechars
+                .iter()
+                .map(|line| line.get(i).unwrap_or(&' '))
+                .collect::<String>();
+            if !n.trim().is_empty() {
+                ns.push(n.trim().parse::<u64>().unwrap());
+            }
+        }
+        // println!("# {ns:?}");
+
+        part2 += match op0 {
+            '*' => ns.iter().product::<u64>(),
+            '+' => ns.iter().sum::<u64>(),
+            _ => panic!(),
+        }
+    }
+
+    println!("{} part 2: {}", title, part2);
 }
 
-const INPUT_DEMO: &str = "";
+const INPUT_DEMO: &str = "123 328  51 64
+ 45 64  387 23
+  6 98  215 314
+*   +   *   +
+";
 
 fn main() {
     run("demo", INPUT_DEMO);
