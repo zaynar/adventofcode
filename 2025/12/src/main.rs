4eda09d64833db53eb92b0b07e76b1105ab853ea
Philip Taylor <philip@zaynar.co.uk>
2025-12-12 12:28:46 +0000

    2025 day 12

--- 2025/12/src/main.rs
+++ 2025/12/src/main.rs
@@ -1,19 +1,83 @@
+// Part 1: 23 mins
+// Part 1+2: 23 mins
+
+use aocgrid::Grid;
+use itertools::Itertools;
+
 fn run(title: &str, input: &str) {
-    let data: Vec<Vec<i32>> = input
-        .lines()
-        .map(|line| {
-            line.split_ascii_whitespace()
-                .map(|n| str::parse(n).unwrap())
-                .collect()
-        })
-        .collect();
+    let segs = input.split("\n\n").collect_vec();
+    let pieces = segs[0..=5].iter().map(|seg| {
+        Grid::from(seg.split_once("\n").unwrap().1).map(|c| if c == '#' { 1 } else { 0 })
+    }).collect_vec();
 
-    println!("{} part 1: {}", title, "TODO");
+    let regions = segs[6].lines().map(|line| {
+        let (sz, cs) = line.split_once(": ").unwrap();
+        let (w, h) = sz.split("x").map(|n| n.parse::<u32>().unwrap()).collect_tuple().unwrap();
+        let cs = cs.split_ascii_whitespace().map(|n| n.parse::<u32>().unwrap()).collect_vec();
+        (w, h, cs)
+    }).collect_vec();
 
-    println!("{} part 2: {}", title, "TODO");
+    let piece_size = pieces.iter().map(|piece| {
+        let mut n = 0;
+        piece.for_each(|x, y, p| n += *p);
+        n
+    }).collect_vec();
+
+    // println!("{pieces:?}");
+    // println!("{regions:?}");
+
+    let mut part1 = 0;
+
+    for r in &regions {
+        let (w, h, cs) = r;
+        let used = cs.iter().enumerate().map(|(i, n)| piece_size[i] * n).sum::<u32>();
+        if used > w * h {
+            println!("{r:?} impossible");
+        } else if (w / 3) * (h / 3) >= cs.iter().sum() {
+            println!("{r:?} trivial");
+            part1 += 1;
+        } else {
+            println!("{r:?}: {}", w * h - used);
+        }
+    }
+
+    println!("{} part 1: {}", title, part1);
 }
 
-const INPUT_DEMO: &str = "";
+const INPUT_DEMO: &str = "0:
+###
+##.
+##.
+
+1:
+###
+##.
+.##
+
+2:
+.##
+###
+##.
+
+3:
+##.
+###
+##.
+
+4:
+###
+#..
+###
+
+5:
+###
+.#.
+###
+
+4x4: 0 0 0 0 2 0
+12x5: 1 0 1 0 2 2
+12x5: 1 0 1 0 3 2
+";
 
 fn main() {
     run("demo", INPUT_DEMO);
